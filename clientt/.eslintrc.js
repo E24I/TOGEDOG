0fdc1aee1ff26c45eec4f53f859d92ab0441176a
kuetiipp <tjstjsgoqgoq@naver.com>
2023-11-06 16:22:05 +0900

    init

---
+++ clientt/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@tanstack-query/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "prettier",
+    "@tanstack-query",
+  ],
+  rules: {},
+};
---
+++ clientt/src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return <div className="App">투게독 시작</div>;
+};
+
+export default App;
