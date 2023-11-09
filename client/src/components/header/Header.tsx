53d33c8b364604359e80eea6e6026d9c9aa9be2a
kuetiipp <tjstjsgoqgoq@naver.com>
2023-11-09 14:15:14 +0900

    :iphone: design: 헤더 컴포넌트 생성

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,22 +1,54 @@
-import React from "react";
-import { ReactComponent as Message } from "../../assets/images/icons/Message.svg";
-import { styled } from "styled-components";
+import React, { useState } from "react";
+import { ReactComponent as Home } from "../../assets/images/icons/headerIcons/Home.svg";
+import { ReactComponent as Main } from "../../assets/images/icons/headerIcons/Main.svg";
+import { ReactComponent as Map } from "../../assets/images/icons/headerIcons/Map.svg";
+import { ReactComponent as CreateFeed } from "../../assets/images/icons/headerIcons/CreateFeed.svg";
+import { ReactComponent as Notifications } from "../../assets/images/icons/headerIcons/Notifications.svg";
+import { ReactComponent as NotificationsRedPoint } from "../../assets/images/icons/headerIcons/NotificationsRedpoint.svg";
+import { ReactComponent as Profile } from "../../assets/images/icons/headerIcons/Profile.svg";
+import {
+  MiddleButtonContainer,
+  HeaderContainer,
+  NotificationsContainer,
+} from "./Header.Style";
+import styled from "styled-components";
 
 const Header: React.FC = () => {
+  const [isRead, setRead] = useState<boolean>(false);
+
+  const convertToRead = () => {
+    if (isRead !== true) {
+      setRead(true);
+    }
+  };
+
   return (
-    <>
-      <Mes />
-    </>
+    <HeaderContainer>
+      <Logo />
+      <MiddleButtonContainer>
+        <MainButtonStyle />
+        <MapButtonStyle />
+        <CreateFeedButtonStyle />
+        <NotificationsContainer onClick={convertToRead}>
+          {isRead === false ? <RedPointStyle /> : <NotificationsStyle />}
+        </NotificationsContainer>
+      </MiddleButtonContainer>
+      <ProfileStyle />
+    </HeaderContainer>
   );
 };
 
-export default Header;
-
-export const Mes = styled(Message)`
-  width: 200px;
-  height: 200px;
-  path {
-    fill: black;
-  }
-  margin-left: 40px;
+//assets style
+export const Logo = styled(Home)``;
+export const MainButtonStyle = styled(Main)``;
+export const MapButtonStyle = styled(Map)``;
+export const CreateFeedButtonStyle = styled(CreateFeed)``;
+export const NotificationsStyle = styled(Notifications)``;
+export const RedPointStyle = styled(NotificationsRedPoint)``;
+export const ProfileStyle = styled(Profile)`
+  padding: 15px;
+  background-color: #d7d7d7;
+  border-radius: 100px;
 `;
+
+export default Header;
