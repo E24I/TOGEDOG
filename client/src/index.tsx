966eaf6ea7f689c4989135b1958edccb8ad508b1
JB0129 <rltkqja1@gmail.com>
2023-12-18 17:57:31 +0900

    📱: 드롭다운 컴포넌트 양식 수정 및 사용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,11 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import "./index.css";
 import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import {
   RecoilRoot,
@@ -11,8 +15,28 @@ import {
   useRecoilState,
   useRecoilValue,
 } from "recoil";
+import { isAxiosError } from "axios";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: true,
+      retry: 0,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error) => {
+      if (isAxiosError(error)) {
+        if (error.response?.status === 500) {
+          // 서버 500 에러
+        }
+        // 토큰 만료 오류 체크
+      }
+    },
+  }),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -21,7 +45,7 @@ root.render(
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools /> */}
     </QueryClientProvider>
   </RecoilRoot>,
 );
