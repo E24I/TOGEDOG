510f8281d37e0cfdc2bf151ef9fffd02bf3e17b3
leesaewoo <ljww123@naver.com>
2023-12-25 14:59:20 +0900

    fix: chat 부분 수정

--- server/src/main/java/togedog/server/domain/chat/dto/ChatRoomResponse.java
+++ server/src/main/java/togedog/server/domain/chat/dto/ChatRoomResponse.java
@@ -1,10 +1,14 @@
 package togedog.server.domain.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatRoomResponse {
 
     private Long chatRoomId;
