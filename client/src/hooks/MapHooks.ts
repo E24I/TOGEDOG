a1a9036f5f7e783ad630e22ce518104048b9d469
JB0129 <rltkqja1@gmail.com>
2023-12-27 18:00:40 +0900

    📱: petmap 좌표 검색 훅 생성

--- client/src/hooks/MapHooks.ts
+++ client/src/hooks/MapHooks.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useMutation } from "@tanstack/react-query";
-import { enrollMapType } from "../types/mapType";
+import { coordinateType, enrollMapType } from "../types/mapType";
 import { postMap } from "../services/mapService";
 import { useNavigate } from "react-router-dom";
+import PetMap from "../pages/PetMap";
 
-//맵 좌표 등록
+// 맵 좌표 등록
 export const usePostMap = (enrollMap: enrollMapType): any => {
   const navigator = useNavigate();
   return useMutation({
@@ -17,6 +18,22 @@ export const usePostMap = (enrollMap: enrollMapType): any => {
     },
     onError: (err) => {
       alert(err);
+      return;
+    },
+  });
+};
+
+export const usePetMap = (coordinate: coordinateType) => {
+  return useMutation({
+    mutationFn: async () => {
+      return PetMap(coordinate);
+    },
+    onSuccess: (res) => {
+      console.log(res);
+      return;
+    },
+    onError: (err) => {
+      console.log(err);
       return;
     },
   });
