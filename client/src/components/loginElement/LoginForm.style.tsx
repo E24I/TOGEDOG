bdc9e570d933def7ea62ea16362285ca504488d0
JB0129 <rltkqja1@gmail.com>
2024-01-11 22:58:14 +0900

    :iphone: style 코드 이동

--- client/src/components/loginElement/LoginForm.style.tsx
+++ client/src/components/loginElement/LoginForm.style.tsx
@@ -69,6 +69,44 @@ export const LoginContainer = styled.div`
   align-items: center;
 `;
 
+export const MainContainer = styled.div`
+  width: 260px;
+  height: calc(100vh - 70px);
+  overflow-y: scroll;
+  position: relative;
+
+  // 스크롤바 설정(넓이 등)
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const MainPictures = styled.ul<{ top?: string }>`
+  margin-top: ${(props) => props.top && props.top};
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+`;
+
+export const MainPicture = styled.li`
+  background-color: rgb(255, 255, 255);
+  width: 240px;
+  height: 300px;
+  margin: 10px 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding-top: 20px;
+`;
+
+export const MainImage = styled.img`
+  background-color: rgb(215, 177, 135);
+  width: 200px;
+  height: 210px;
+`;
+
 export const InputContainer = styled.div`
   background-color: white;
   border-radius: 30px;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { styled } from "styled-components";
 import LoginForm from "../components/loginElement/LoginForm";
-import { LoginContainer } from "../components/loginElement/LoginForm.style";
+import {
+  LoginContainer,
+  MainContainer,
+  MainImage,
+  MainPicture,
+  MainPictures,
+} from "../components/loginElement/LoginForm.style";
 
 const Login: React.FC = () => {
   return (
@@ -30,41 +35,3 @@ const Login: React.FC = () => {
 };
 
 export default Login;
-
-export const MainContainer = styled.div`
-  width: 260px;
-  height: calc(100vh - 70px);
-  overflow-y: scroll;
-  position: relative;
-
-  // 스크롤바 설정(넓이 등)
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-export const MainPictures = styled.ul<{ top?: string }>`
-  margin-top: ${(props) => props.top && props.top};
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
-`;
-
-export const MainPicture = styled.li`
-  background-color: rgb(255, 255, 255);
-  width: 240px;
-  height: 300px;
-  margin: 10px 0px;
-  display: flex;
-  flex-direction: column;
-  justify-content: start;
-  align-items: center;
-  padding-top: 20px;
-`;
-
-export const MainImage = styled.img`
-  background-color: rgb(215, 177, 135);
-  width: 200px;
-  height: 210px;
-`;
